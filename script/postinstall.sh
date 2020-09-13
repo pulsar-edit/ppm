d04fe29a3bf3afcd1c065d0d7ea7f2b1ff7336c0
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2020-09-13 18:28:23 -0500

    parallel node-gyp
    Setting JOBS makes node-gyp parallel
    
    https://github.com/nodejs/node-gyp/blob/9bf112b44c98be39bf8770a1341fa4ddf23556d4/lib/build.js#L24

--- script/postinstall.sh
+++ script/postinstall.sh
@@ -7,6 +7,10 @@ node script/download-node.js
 
 echo
 echo ">> Rebuilding apm dependencies with bundled Node $(./bin/node -p "process.version + ' ' + process.arch")"
+
+# parallel node-gyp
+JOBS=16
+
 ./bin/npm rebuild
 
 echo
