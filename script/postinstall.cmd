94551a560db832a09803dbd9dd6d16ab7a402f57
DeeDeeG <DeeDeeG@users.noreply.github.com>
2021-01-12 21:51:04 -0500

    script\postinstall.cmd: Use batch comment syntax
    :: is the comment indicator in batch scripts,
    not # like in most languages.

--- script/postinstall.cmd
+++ script/postinstall.cmd
@@ -9,7 +9,7 @@ echo.
 for /f "delims=" %%i in ('.\bin\node.exe -p "process.version + ' ' + process.arch"') do set bundledVersion=%%i
 echo ^>^> Rebuilding apm dependencies with bundled Node !bundledVersion!
 
-# parallel node-gyp
+:: parallel node-gyp
 setx JOBS 16
 
 call .\bin\npm.cmd rebuild
