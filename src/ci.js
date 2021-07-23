0755b3cad6f9552a84e64086bec7a9ad0507bdf9
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-07-22 22:37:20 -0500

    feat: convert ci to js

---
+++ src/ci.js
@@ -0,0 +1,94 @@
+/*
+ * decaffeinate suggestions:
+ * DS101: Remove unnecessary use of Array.from
+ * DS102: Remove unnecessary code created because of implicit returns
+ * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
+ */
+import path from "path"
+import fs from "./fs"
+import yargs from "yargs"
+import async from "async"
+import _ from "underscore-plus"
+import config from "./apm"
+import Command from "./command"
+
+export default class Ci extends Command {
+  constructor() {
+    super()
+    this.atomDirectory = config.getAtomDirectory()
+    this.atomNodeDirectory = path.join(this.atomDirectory, ".node-gyp")
+    this.atomNpmPath = require.resolve("npm/bin/npm-cli")
+  }
+
+  parseOptions(argv) {
+    const options = yargs(argv).wrap(Math.min(100, yargs.terminalWidth()))
+    options.usage(`\
+Usage: apm ci
+
+Install a package with a clean slate.
+
+If you have an up-to-date package-lock.json file created by apm install,
+apm ci will install its locked contents exactly. It is substantially
+faster than apm install and produces consistently reproduceable builds,
+but cannot be used to install new packages or dependencies.\
+`)
+
+    options.alias("h", "help").describe("help", "Print this usage message")
+    return options.boolean("verbose").default("verbose", false).describe("verbose", "Show verbose debug information")
+  }
+
+  installModules(options, callback) {
+    process.stdout.write("Installing locked modules")
+    if (options.argv.verbose) {
+      process.stdout.write("\n")
+    } else {
+      process.stdout.write(" ")
+    }
+
+    const installArgs = [
+      "ci",
+      "--globalconfig",
+      config.getGlobalConfigPath(),
+      "--userconfig",
+      config.getUserConfigPath(),
+      ...Array.from(this.getNpmBuildFlags()),
+    ]
+    if (options.argv.verbose) {
+      installArgs.push("--verbose")
+    }
+
+    fs.makeTreeSync(this.atomDirectory)
+
+    const env = _.extend({}, process.env, { HOME: this.atomNodeDirectory, RUSTUP_HOME: config.getRustupHomeDirPath() })
+    this.addBuildEnvVars(env)
+
+    const installOptions = { env, streaming: options.argv.verbose }
+
+    return this.fork(this.atomNpmPath, installArgs, installOptions, (...args) => {
+      return this.logCommandResults(callback, ...Array.from(args))
+    })
+  }
+
+  run(options) {
+    const { callback } = options
+    const opts = this.parseOptions(options.commandArgs)
+
+    const commands = []
+    commands.push((callback) => {
+      return config.loadNpm((error, npm) => {
+        this.npm = npm
+        return callback(error)
+      })
+    })
+    commands.push((cb) => this.loadInstalledAtomMetadata(cb))
+    commands.push((cb) => this.installModules(opts, cb))
+
+    const iteratee = (item, next) => item(next)
+    return async.mapSeries(commands, iteratee, function (err) {
+      if (err) {
+        return callback(err)
+      }
+      return callback(null)
+    })
+  }
+}
