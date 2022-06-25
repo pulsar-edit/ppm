201114b92a0a94a3446e335fba2c955956e718f5
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-06-24 23:58:12 -0700

    fix: add the paths to the bundled tools in the environment variables

--- bin/npm.cmd
+++ bin/npm.cmd
@@ -3,8 +3,11 @@ setlocal EnableDelayedExpansion
 
 set "PATH=%~dp0;%PATH%"
 
-:: set the path to the node-gyp fallback
+:: add the paths to the bundled tools
 set npm_config_node_gyp="%~dp0\\..\\node_modules\\node-gyp\\bin\\node-gyp.js"
 set "npm_config_node_gyp=%npm_config_node_gyp:\=/%"
+set node_gyp_cmd="%~dp0\..\node_modules\.bin\node-gyp.cmd"
+set "Path=%node_gyp_cmd%;%Path%"
+set "Path=%~dp0;%Path%"
 
 "%~dp0\..\node_modules\.bin\npm.cmd" %*
