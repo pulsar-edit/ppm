f965e13d7968071a77c1e035566fd637c1897b2f
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2022-06-25 00:05:09 -0700

    fix: remove node-gyp paths from script files

--- bin/apm.cmd
+++ bin/apm.cmd
@@ -15,8 +15,6 @@ if not defined apm_git_path (
 :: add the paths to the bundled tools
 set npm_config_node_gyp="%~dp0\\..\\node_modules\\node-gyp\\bin\\node-gyp.js"
 set "npm_config_node_gyp=%npm_config_node_gyp:\=/%"
-set node_gyp_cmd="%~dp0\..\node_modules\.bin\node-gyp.cmd"
-set "Path=%node_gyp_cmd%;%Path%"
 set "Path=%~dp0;%Path%"
 
 if exist "%~dp0\node.exe" (
--- bin/npm.cmd
+++ bin/npm.cmd
@@ -6,8 +6,6 @@ set "PATH=%~dp0;%PATH%"
 :: add the paths to the bundled tools
 set npm_config_node_gyp="%~dp0\\..\\node_modules\\node-gyp\\bin\\node-gyp.js"
 set "npm_config_node_gyp=%npm_config_node_gyp:\=/%"
-set node_gyp_cmd="%~dp0\..\node_modules\.bin\node-gyp.cmd"
-set "Path=%node_gyp_cmd%;%Path%"
 set "Path=%~dp0;%Path%"
 
 "%~dp0\..\node_modules\.bin\npm.cmd" %*
