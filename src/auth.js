f8844fdc441fca5d3c8f2b172d7a3c760128cc1c
Sertonix <83883937+Sertonix@users.noreply.github.com>
2022-11-04 08:31:53 +0100

    added comments to src/auth.js

--- src/auth.js
+++ src/auth.js
@@ -2,6 +2,7 @@ let keytar;
 try {
   keytar = require('keytar');
 } catch (error) {
+  // Gracefully handle keytar failing to load due to missing library on Linux
   if (process.platform !== 'linux') throw error;
   keytar = {
     findPassword: () => Promise.reject(),
@@ -12,6 +13,10 @@ try {
 const tokenName = 'Atom.io API Token';
 
 module.exports = {
+  // Get the package API token from the keychain.
+  //
+  // callback - A function to call with an error as the first argument and a
+  //            string token as the second argument.
   getToken(callback) {
     keytar.findPassword(tokenName).then( token => {
       if (token) {
@@ -28,5 +33,8 @@ module.exports = {
       }
     });
   },
+  // Save the given token to the keychain.
+  //
+  // token - A string token to save.
   saveToken: token => keytar.setPassword(tokenName, 'atom.io', token)
 };
