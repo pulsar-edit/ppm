df5a92564837f07f18a31c6064dc5d6eff716ce5
GuilleW <devguillew@free.fr>
2022-11-07 00:13:07 +0100

    decaffeinate spec/spec-helper

---
+++ spec/spec-helper.js
@@ -0,0 +1,20 @@
+const auth = require('../lib/auth')
+
+global.silenceOutput = function (callThrough) {
+  if (callThrough) { callThrough = false }
+  spyOn(console, 'log')
+  spyOn(console, 'error')
+  spyOn(process.stdout, 'write')
+  spyOn(process.stderr, 'write')
+
+  if (callThrough) {
+    return ([
+      console.log,
+      console.error,
+      process.stdout.write,
+      process.stderr.write
+    ]).map((spy) => spy.andCallThrough())
+  }
+}
+
+global.spyOnToken = () => spyOn(auth, 'getToken').andCallFake(callback => callback(null, 'token'))
