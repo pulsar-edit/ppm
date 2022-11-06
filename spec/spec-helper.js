cda8b9b901d1b2bb1fcc23ce281fe4daa7f5df9d
GuilleW <devguillew@free.fr>
2022-11-07 00:13:07 +0100

    decaffeinate spec/spec-helper

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -1,17 +1,11 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
-const auth = require('../lib/auth');
+const auth = require('../lib/auth')
 
-global.silenceOutput = function(callThrough) {
-  if (callThrough == null) { callThrough = false; }
-  spyOn(console, 'log');
-  spyOn(console, 'error');
-  spyOn(process.stdout, 'write');
-  spyOn(process.stderr, 'write');
+global.silenceOutput = function (callThrough) {
+  if (callThrough) { callThrough = false }
+  spyOn(console, 'log')
+  spyOn(console, 'error')
+  spyOn(process.stdout, 'write')
+  spyOn(process.stderr, 'write')
 
   if (callThrough) {
     return ([
@@ -19,8 +13,8 @@ global.silenceOutput = function(callThrough) {
       console.error,
       process.stdout.write,
       process.stderr.write
-    ]).map((spy) => spy.andCallThrough());
+    ]).map((spy) => spy.andCallThrough())
   }
-};
+}
 
-global.spyOnToken = () => spyOn(auth, 'getToken').andCallFake(callback => callback(null, 'token'));
+global.spyOnToken = () => spyOn(auth, 'getToken').andCallFake(callback => callback(null, 'token'))
