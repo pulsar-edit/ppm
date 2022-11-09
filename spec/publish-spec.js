72015915a5e474090550ad2d0070c94a1a418cb1
GuilleW <devguillew@free.fr>
2022-11-10 00:58:11 +0100

    Decaffeinate `spec/publish-spec`

--- spec/publish-spec.js
+++ spec/publish-spec.js
@@ -1,92 +1,87 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
-const path = require('path');
-const fs = require('fs-plus');
-const temp = require('temp');
-const express = require('express');
-const http = require('http');
-const apm = require('../lib/apm-cli');
+const path = require('path')
+const fs = require('fs-plus')
+const temp = require('temp')
+const express = require('express')
+const http = require('http')
+const apm = require('../lib/apm-cli')
 
-describe('apm publish', function() {
-  let [server] = Array.from([]);
+describe('apm publish', () => {
+  var server
+  server = [][0]
 
-  beforeEach(function() {
-    spyOnToken();
-    silenceOutput();
+  beforeEach(() => {
+    spyOnToken()
+    silenceOutput()
+    const app = express()
+    server = http.createServer(app)
+    var live = false
+    server.listen(3000, '127.0.0.1', () => {
+      const atomHome = temp.mkdirSync('apm-home-dir-')
+      process.env.ATOM_HOME = atomHome
+      process.env.ATOM_API_URL = 'http://localhost:3000/api'
+      process.env.ATOM_RESOURCE_PATH = temp.mkdirSync('atom-resource-path-')
+      live = true
+    })
+    waitsFor(() => live)
+  })
 
-    const app = express();
-    server =  http.createServer(app);
+  afterEach(() => {
+    var done = false
+    server.close(() => {
+      done = true
+    })
+    waitsFor(() => done)
+  })
 
-    let live = false;
-    server.listen(3000, '127.0.0.1', function() {
-      const atomHome = temp.mkdirSync('apm-home-dir-');
-      process.env.ATOM_HOME = atomHome;
-      process.env.ATOM_API_URL = "http://localhost:3000/api";
-      process.env.ATOM_RESOURCE_PATH = temp.mkdirSync('atom-resource-path-');
-      return live = true;
-    });
-    return waitsFor(() => live);
-  });
+  it("validates the package's package.json file", () => {
+    const packageToPublish = temp.mkdirSync('apm-test-package-')
+    fs.writeFileSync(path.join(packageToPublish, 'package.json'), '}{')
+    process.chdir(packageToPublish)
+    const callback = jasmine.createSpy('callback')
+    apm.run(['publish'], callback)
+    waitsFor('waiting for publish to complete', 600000, () => callback.callCount === 1)
+    runs(() => {
+      expect(callback.mostRecentCall.args[0].message).toBe('Error parsing package.json file: Unexpected token } in JSON at position 0')
+    })
+  })
 
-  afterEach(function() {
-    let done = false;
-    server.close(() => done = true);
-    return waitsFor(() => done);
-  });
-
-  it("validates the package's package.json file", function() {
-    const packageToPublish = temp.mkdirSync('apm-test-package-');
-    fs.writeFileSync(path.join(packageToPublish, 'package.json'), '}{');
-    process.chdir(packageToPublish);
-    const callback = jasmine.createSpy('callback');
-    apm.run(['publish'], callback);
-
-    waitsFor('waiting for publish to complete', 600000, () => callback.callCount === 1);
-
-    return runs(() => expect(callback.mostRecentCall.args[0].message).toBe('Error parsing package.json file: Unexpected token } in JSON at position 0'));
-  });
-
-  it("validates the package is in a Git repository", function() {
-    const packageToPublish = temp.mkdirSync('apm-test-package-');
+  it('validates the package is in a Git repository', () => {
+    const packageToPublish = temp.mkdirSync('apm-test-package-')
     const metadata = {
       name: 'test',
       version: '1.0.0'
-    };
-    fs.writeFileSync(path.join(packageToPublish, 'package.json'), JSON.stringify(metadata));
-    process.chdir(packageToPublish);
-    const callback = jasmine.createSpy('callback');
-    apm.run(['publish'], callback);
+    }
+    fs.writeFileSync(path.join(packageToPublish, 'package.json'), JSON.stringify(metadata))
+    process.chdir(packageToPublish)
+    const callback = jasmine.createSpy('callback')
+    apm.run(['publish'], callback)
+    waitsFor('waiting for publish to complete', 600000, () => callback.callCount === 1)
+    runs(() => {
+      expect(callback.mostRecentCall.args[0].message).toBe('Package must be in a Git repository before publishing: https://help.github.com/articles/create-a-repo')
+    })
+  })
 
-    waitsFor('waiting for publish to complete', 600000, () => callback.callCount === 1);
-
-    return runs(() => expect(callback.mostRecentCall.args[0].message).toBe('Package must be in a Git repository before publishing: https://help.github.com/articles/create-a-repo'));
-  });
-
-  it("validates the engines.atom range in the package.json file", function() {
-    const packageToPublish = temp.mkdirSync('apm-test-package-');
+  it('validates the engines.atom range in the package.json file', () => {
+    const packageToPublish = temp.mkdirSync('apm-test-package-')
     const metadata = {
       name: 'test',
       version: '1.0.0',
       engines: {
         atom: '><>'
       }
-    };
-    fs.writeFileSync(path.join(packageToPublish, 'package.json'), JSON.stringify(metadata));
-    process.chdir(packageToPublish);
-    const callback = jasmine.createSpy('callback');
-    apm.run(['publish'], callback);
+    }
+    fs.writeFileSync(path.join(packageToPublish, 'package.json'), JSON.stringify(metadata))
+    process.chdir(packageToPublish)
+    const callback = jasmine.createSpy('callback')
+    apm.run(['publish'], callback)
+    waitsFor('waiting for publish to complete', 600000, () => callback.callCount === 1)
+    runs(() => {
+      expect(callback.mostRecentCall.args[0].message).toBe('The Pulsar or Atom engine range in the package.json file is invalid: ><>')
+    })
+  })
 
-    waitsFor('waiting for publish to complete', 600000, () => callback.callCount === 1);
-
-    return runs(() => expect(callback.mostRecentCall.args[0].message).toBe('The Pulsar or Atom engine range in the package.json file is invalid: ><>'));
-  });
-
-  it("validates the dependency semver ranges in the package.json file", function() {
-    const packageToPublish = temp.mkdirSync('apm-test-package-');
+  it('validates the dependency semver ranges in the package.json file', () => {
+    const packageToPublish = temp.mkdirSync('apm-test-package-')
     const metadata = {
       name: 'test',
       version: '1.0.0',
@@ -98,19 +93,19 @@ describe('apm publish', function() {
         abcd: 'latest',
         foo: '^^'
       }
-    };
-    fs.writeFileSync(path.join(packageToPublish, 'package.json'), JSON.stringify(metadata));
-    process.chdir(packageToPublish);
-    const callback = jasmine.createSpy('callback');
-    apm.run(['publish'], callback);
+    }
+    fs.writeFileSync(path.join(packageToPublish, 'package.json'), JSON.stringify(metadata))
+    process.chdir(packageToPublish)
+    const callback = jasmine.createSpy('callback')
+    apm.run(['publish'], callback)
+    waitsFor('waiting for publish to complete', 600000, () => callback.callCount === 1)
+    runs(() => {
+      expect(callback.mostRecentCall.args[0].message).toBe('The foo dependency range in the package.json file is invalid: ^^')
+    })
+  })
 
-    waitsFor('waiting for publish to complete', 600000, () => callback.callCount === 1);
-
-    return runs(() => expect(callback.mostRecentCall.args[0].message).toBe('The foo dependency range in the package.json file is invalid: ^^'));
-  });
-
-  return it("validates the dev dependency semver ranges in the package.json file", function() {
-    const packageToPublish = temp.mkdirSync('apm-test-package-');
+  it('validates the dev dependency semver ranges in the package.json file', () => {
+    const packageToPublish = temp.mkdirSync('apm-test-package-')
     const metadata = {
       name: 'test',
       version: '1.0.0',
@@ -125,14 +120,14 @@ describe('apm publish', function() {
         abcd: 'latest',
         bar: '1,3'
       }
-    };
-    fs.writeFileSync(path.join(packageToPublish, 'package.json'), JSON.stringify(metadata));
-    process.chdir(packageToPublish);
-    const callback = jasmine.createSpy('callback');
-    apm.run(['publish'], callback);
-
-    waitsFor('waiting for publish to complete', 600000, () => callback.callCount === 1);
-
-    return runs(() => expect(callback.mostRecentCall.args[0].message).toBe('The bar dev dependency range in the package.json file is invalid: 1,3'));
-  });
-});
+    }
+    fs.writeFileSync(path.join(packageToPublish, 'package.json'), JSON.stringify(metadata))
+    process.chdir(packageToPublish)
+    const callback = jasmine.createSpy('callback')
+    apm.run(['publish'], callback)
+    waitsFor('waiting for publish to complete', 600000, () => callback.callCount === 1)
+    runs(() => {
+      expect(callback.mostRecentCall.args[0].message).toBe('The bar dev dependency range in the package.json file is invalid: 1,3')
+    })
+  })
+})
