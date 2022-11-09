8bee9ca3a099c824305a485838f5b38f6d01e434
GuilleW <devguillew@free.fr>
2022-11-10 00:35:38 +0100

    Decaffeinate `spec/list-spec`

--- spec/list-spec.js
+++ spec/list-spec.js
@@ -1,53 +1,53 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * DS205: Consider reworking code to avoid use of IIFEs
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
-const path = require('path');
-const fs = require('fs-plus');
-const temp = require('temp');
-const wrench = require('wrench');
-const apm = require('../lib/apm-cli');
-const CSON = require('season');
+const path = require('path')
+const fs = require('fs-plus')
+const temp = require('temp')
+const wrench = require('wrench')
+const apm = require('../lib/apm-cli')
+const CSON = require('season')
 
-const listPackages = function(args, doneCallback) {
-  const callback = jasmine.createSpy('callback');
-  apm.run(['list'].concat(args), callback);
+const listPackages = (args, doneCallback) => {
+  const callback = jasmine.createSpy('callback')
+  apm.run(['list'].concat(args), callback)
+  waitsFor(() => callback.callCount === 1)
+  runs(doneCallback)
+}
 
-  waitsFor(() => callback.callCount === 1);
+const createFakePackage = (type, metadata) => {
+  const packagesFolder = (() => {
+    switch (type) {
+      case 'user':
+      case 'git':
+        return 'packages'
+      case 'dev':
+        return path.join('dev', 'packages')
+    }
+  })()
+  const targetFolder = path.join(process.env.ATOM_HOME, packagesFolder, metadata.name)
+  fs.makeTreeSync(targetFolder)
+  fs.writeFileSync(path.join(targetFolder, 'package.json'), JSON.stringify(metadata))
+}
 
-  return runs(doneCallback);
-};
+const removeFakePackage = (type, name) => {
+  const packagesFolder = (() => {
+    switch (type) {
+      case 'user':
+      case 'git':
+        return 'packages'
+      case 'dev':
+        return path.join('dev', 'packages')
+    }
+  })()
+  const targetFolder = path.join(process.env.ATOM_HOME, packagesFolder, name)
+  fs.removeSync(targetFolder)
+}
 
-const createFakePackage = function(type, metadata) {
-  const packagesFolder = (() => { switch (type) {
-    case "user": case "git": return "packages";
-    case "dev": return path.join("dev", "packages");
-  } })();
-  const targetFolder = path.join(process.env.ATOM_HOME, packagesFolder, metadata.name);
-  fs.makeTreeSync(targetFolder);
-  return fs.writeFileSync(path.join(targetFolder, 'package.json'), JSON.stringify(metadata));
-};
+describe('apm list', () => {
+  var atomHome, resourcePath
 
-const removeFakePackage = function(type, name) {
-  const packagesFolder = (() => { switch (type) {
-    case "user": case "git": return "packages";
-    case "dev": return path.join("dev", "packages");
-  } })();
-  const targetFolder = path.join(process.env.ATOM_HOME, packagesFolder, name);
-  return fs.removeSync(targetFolder);
-};
-
-describe('apm list', function() {
-  let [resourcePath, atomHome] = Array.from([]);
-
-  beforeEach(function() {
-    silenceOutput();
-    spyOnToken();
-
-    resourcePath = temp.mkdirSync('apm-resource-path-');
+  beforeEach(() => {
+    silenceOutput()
+    spyOnToken()
+    resourcePath = temp.mkdirSync('apm-resource-path-')
     const atomPackages = {
       'test-module': {
         metadata: {
@@ -55,145 +55,185 @@ describe('apm list', function() {
           version: '1.0.0'
         }
       }
-    };
-    fs.writeFileSync(path.join(resourcePath, 'package.json'), JSON.stringify({_atomPackages: atomPackages}));
-    process.env.ATOM_RESOURCE_PATH = resourcePath;
-    atomHome = temp.mkdirSync('apm-home-dir-');
-    process.env.ATOM_HOME = atomHome;
-
-    createFakePackage("user", {
-      name: "user-package",
-      version: "1.0.0"
     }
-    );
-    createFakePackage("dev", {
-      name: "dev-package",
-      version: "1.0.0"
-    }
-    );
-    createFakePackage("git", {
-      name: "git-package",
-      version: "1.0.0",
+    fs.writeFileSync(path.join(resourcePath, 'package.json'), JSON.stringify({
+      _atomPackages: atomPackages
+    }))
+    process.env.ATOM_RESOURCE_PATH = resourcePath
+    atomHome = temp.mkdirSync('apm-home-dir-')
+    process.env.ATOM_HOME = atomHome
+    createFakePackage('user', {
+      name: 'user-package',
+      version: '1.0.0'
+    })
+    createFakePackage('dev', {
+      name: 'dev-package',
+      version: '1.0.0'
+    })
+    createFakePackage('git', {
+      name: 'git-package',
+      version: '1.0.0',
       apmInstallSource: {
-        type: "git",
-        source: "git+ssh://git@github.com:user/repo.git",
-        sha: "abcdef1234567890"
+        type: 'git',
+        source: 'git+ssh://git@github.com:user/repo.git',
+        sha: 'abcdef1234567890'
       }
-    }
-    );
+    })
+    const badPackagePath = path.join(process.env.ATOM_HOME, 'packages', '.bin')
+    fs.makeTreeSync(badPackagePath)
+    fs.writeFileSync(path.join(badPackagePath, 'file.txt'), 'some fake stuff')
+  })
 
-    const badPackagePath = path.join(process.env.ATOM_HOME, "packages", ".bin");
-    fs.makeTreeSync(badPackagePath);
-    return fs.writeFileSync(path.join(badPackagePath, "file.txt"), "some fake stuff");
-  });
+  it('lists the installed packages', () => {
+    listPackages([], () => {
+      const lines = console.log.argsForCall.map(arr => arr.join(' '))
+      expect(lines[0]).toMatch(/Built-in Atom Packages.*1/)
+      expect(lines[1]).toMatch(/test-module@1\.0\.0/)
+      expect(lines[3]).toMatch(/Dev Packages.*1/)
+      expect(lines[4]).toMatch(/dev-package@1\.0\.0/)
+      expect(lines[6]).toMatch(/Community Packages.*1/)
+      expect(lines[7]).toMatch(/user-package@1\.0\.0/)
+      expect(lines[9]).toMatch(/Git Packages.*1/)
+      expect(lines[10]).toMatch(/git-package@1\.0\.0/)
+      expect(lines.join('\n')).not.toContain('.bin')
+    })
+  })
 
-  it('lists the installed packages', () => listPackages([], function() {
-    const lines = console.log.argsForCall.map(arr => arr.join(' '));
-    expect(lines[0]).toMatch(/Built-in Atom Packages.*1/);
-    expect(lines[1]).toMatch(/test-module@1\.0\.0/);
-    expect(lines[3]).toMatch(/Dev Packages.*1/);
-    expect(lines[4]).toMatch(/dev-package@1\.0\.0/);
-    expect(lines[6]).toMatch(/Community Packages.*1/);
-    expect(lines[7]).toMatch(/user-package@1\.0\.0/);
-    expect(lines[9]).toMatch(/Git Packages.*1/);
-    expect(lines[10]).toMatch(/git-package@1\.0\.0/);
-    return expect(lines.join("\n")).not.toContain('.bin');
-  })); // ensure invalid packages aren't listed
+  it('lists the installed packages without versions with --no-versions', () => {
+    listPackages(['--no-versions'], () => {
+      const lines = console.log.argsForCall.map(arr => arr.join(' '))
+      expect(lines[0]).toMatch(/Built-in Atom Packages.*1/)
+      expect(lines[1]).toMatch(/test-module/)
+      expect(lines[3]).toMatch(/Dev Packages.*1/)
+      expect(lines[4]).toMatch(/dev-package/)
+      expect(lines[6]).toMatch(/Community Packages.*1/)
+      expect(lines[7]).toMatch(/user-package/)
+      expect(lines[9]).toMatch(/Git Packages.*1/)
+      expect(lines[10]).toMatch(/git-package/)
+      expect(lines.join('\n')).not.toContain('.bin')
+    })
+  })
 
-  it('lists the installed packages without versions with --no-versions', () => listPackages(['--no-versions'], function() {
-    const lines = console.log.argsForCall.map(arr => arr.join(' '));
-    expect(lines[0]).toMatch(/Built-in Atom Packages.*1/);
-    expect(lines[1]).toMatch(/test-module/);
-    expect(lines[3]).toMatch(/Dev Packages.*1/);
-    expect(lines[4]).toMatch(/dev-package/);
-    expect(lines[6]).toMatch(/Community Packages.*1/);
-    expect(lines[7]).toMatch(/user-package/);
-    expect(lines[9]).toMatch(/Git Packages.*1/);
-    expect(lines[10]).toMatch(/git-package/);
-    return expect(lines.join("\n")).not.toContain('.bin');
-  })); // ensure invalid packages aren't listed
+  describe('enabling and disabling packages', () => {
+    beforeEach(() => {
+      const packagesPath = path.join(atomHome, 'packages')
+      fs.makeTreeSync(packagesPath)
+      wrench.copyDirSyncRecursive(path.join(__dirname, 'fixtures', 'test-module'), path.join(packagesPath, 'test-module'))
+      const configPath = path.join(atomHome, 'config.cson')
+      CSON.writeFileSync(configPath, {
+        '*': {
+          core: {
+            disabledPackages: ['test-module']
+          }
+        }
+      })
+    })
 
-  describe('enabling and disabling packages', function() {
-    beforeEach(function() {
-      const packagesPath = path.join(atomHome, 'packages');
-      fs.makeTreeSync(packagesPath);
-      wrench.copyDirSyncRecursive(path.join(__dirname, 'fixtures', 'test-module'), path.join(packagesPath, 'test-module'));
-      const configPath = path.join(atomHome, 'config.cson');
-      return CSON.writeFileSync(configPath, { '*': {
-        core: {disabledPackages: ["test-module"]}
+    it('labels disabled packages', () => {
+      listPackages([], () => {
+        expect(console.log.argsForCall[1][0]).toContain('test-module@1.0.0 (disabled)')
+      })
+    })
+
+    it('displays only disabled packages when --disabled is called', () => {
+      listPackages(['--disabled'], () => {
+        expect(console.log.argsForCall[1][0]).toMatch(/test-module@1\.0\.0$/)
+        expect(console.log.argsForCall.toString()).not.toContain(['user-package'])
+      })
+    })
+
+    it('displays only enabled packages when --enabled is called', () => {
+      listPackages(['--enabled'], () => {
+        expect(console.log.argsForCall[7][0]).toMatch(/user-package@1\.0\.0$/)
+        expect(console.log.argsForCall.toString()).not.toContain(['test-module'])
+      })
+    })
+  })
+
+  it('lists packages in json format when --json is passed', () => {
+    listPackages(['--json'], () => {
+      const json = JSON.parse(console.log.argsForCall[0][0])
+      const apmInstallSource = {
+        type: 'git',
+        source: 'git+ssh://git@github.com:user/repo.git',
+        sha: 'abcdef1234567890'
       }
-    });});
+      expect(json.core).toEqual([
+        {
+          name: 'test-module',
+          version: '1.0.0'
+        }
+      ])
+      expect(json.dev).toEqual([
+        {
+          name: 'dev-package',
+          version: '1.0.0'
+        }
+      ])
+      expect(json.git).toEqual([
+        {
+          name: 'git-package',
+          version: '1.0.0',
+          apmInstallSource: apmInstallSource
+        }
+      ])
+      expect(json.user).toEqual([
+        {
+          name: 'user-package',
+          version: '1.0.0'
+        }
+      ])
+    })
+  })
 
-    it('labels disabled packages', () => listPackages([], () => expect(console.log.argsForCall[1][0]).toContain('test-module@1.0.0 (disabled)')));
+  it('lists packages in bare format when --bare is passed', () => {
+    listPackages(['--bare'], () => {
+      const lines = console.log.argsForCall.map(arr => arr.join(' '))
+      expect(lines[0]).toMatch(/test-module@1\.0\.0/)
+      expect(lines[1]).toMatch(/dev-package@1\.0\.0/)
+      expect(lines[2]).toMatch(/user-package@1\.0\.0/)
+      expect(lines[3]).toMatch(/git-package@1\.0\.0/)
+    })
+  })
 
-    it('displays only disabled packages when --disabled is called', () => listPackages(['--disabled'], function() {
-      expect(console.log.argsForCall[1][0]).toMatch(/test-module@1\.0\.0$/);
-      return expect(console.log.argsForCall.toString()).not.toContain(['user-package']);
-  }));
+  it('list packages in bare format without versions when --bare --no-versions is passed', () => {
+    listPackages(['--bare', '--no-versions'], () => {
+      const lines = console.log.argsForCall.map(arr => arr.join(' '))
+      expect(lines[0]).toMatch(/test-module/)
+      expect(lines[1]).toMatch(/dev-package/)
+      expect(lines[2]).toMatch(/user-package/)
+      expect(lines[3]).toMatch(/git-package/)
+    })
+  })
 
-    return it('displays only enabled packages when --enabled is called', () => listPackages(['--enabled'], function() {
-      expect(console.log.argsForCall[7][0]).toMatch(/user-package@1\.0\.0$/);
-      return expect(console.log.argsForCall.toString()).not.toContain(['test-module']);
-  }));
-});
+  describe('when a section is empty', () => {
+    it('does not list anything for Dev and Git sections', () => {
+      removeFakePackage('git', 'git-package')
+      removeFakePackage('dev', 'dev-package')
+      listPackages([], () => {
+        const output = console.log.argsForCall.map(arr => arr.join(' ')).join('\n')
+        expect(output).not.toMatch(/Git Packages/)
+        expect(output).not.toMatch(/git-package/)
+        expect(output).not.toMatch(/Dev Packages.*1/)
+        expect(output).not.toMatch(/dev-package@1\.0\.0/)
+        expect(output).not.toMatch(/(empty)/)
+      })
+    })
 
-  it('lists packages in json format when --json is passed', () => listPackages(['--json'], function() {
-    const json = JSON.parse(console.log.argsForCall[0][0]);
-    const apmInstallSource = {
-      type: 'git',
-      source: 'git+ssh://git@github.com:user/repo.git',
-      sha: 'abcdef1234567890'
-    };
-    expect(json.core).toEqual([{name: 'test-module', version: '1.0.0'}]);
-    expect(json.dev).toEqual([{name: 'dev-package', version: '1.0.0'}]);
-    expect(json.git).toEqual([{name: 'git-package', version: '1.0.0', apmInstallSource}]);
-    return expect(json.user).toEqual([{name: 'user-package', version: '1.0.0'}]);
-}));
-
-  it('lists packages in bare format when --bare is passed', () => listPackages(['--bare'], function() {
-    const lines = console.log.argsForCall.map(arr => arr.join(' '));
-    expect(lines[0]).toMatch(/test-module@1\.0\.0/);
-    expect(lines[1]).toMatch(/dev-package@1\.0\.0/);
-    expect(lines[2]).toMatch(/user-package@1\.0\.0/);
-    return expect(lines[3]).toMatch(/git-package@1\.0\.0/);
-  }));
-
-  it('list packages in bare format without versions when --bare --no-versions is passed', () => listPackages(['--bare', '--no-versions'], function() {
-    const lines = console.log.argsForCall.map(arr => arr.join(' '));
-    expect(lines[0]).toMatch(/test-module/);
-    expect(lines[1]).toMatch(/dev-package/);
-    expect(lines[2]).toMatch(/user-package/);
-    return expect(lines[3]).toMatch(/git-package/);
-  }));
-
-  return describe('when a section is empty', function() {
-    it('does not list anything for Dev and Git sections', function() {
-      removeFakePackage('git', 'git-package');
-      removeFakePackage('dev', 'dev-package');
-      return listPackages([], function() {
-        const output = console.log.argsForCall.map(arr => arr.join(' ')).join('\n');
-        expect(output).not.toMatch(/Git Packages/);
-        expect(output).not.toMatch(/git-package/);
-        expect(output).not.toMatch(/Dev Packages.*1/);
-        expect(output).not.toMatch(/dev-package@1\.0\.0/);
-        return expect(output).not.toMatch(/(empty)/);
-      });
-    });
-
-    return it('displays "empty" for User section', function() {
-      removeFakePackage('user', 'user-package');
-      return listPackages([], function() {
-        const lines = console.log.argsForCall.map(arr => arr.join(' '));
-        expect(lines[0]).toMatch(/Built-in Atom Packages.*1/);
-        expect(lines[1]).toMatch(/test-module@1\.0\.0/);
-        expect(lines[3]).toMatch(/Dev Packages.*1/);
-        expect(lines[4]).toMatch(/dev-package@1\.0\.0/);
-        expect(lines[6]).toMatch(/Community Packages.*0/);
-        expect(lines[7]).toMatch(/(empty)/);
-        expect(lines[9]).toMatch(/Git Packages.*1/);
-        expect(lines[10]).toMatch(/git-package@1\.0\.0/);
-        return expect(lines.join("\n")).not.toContain('.bin');
-      });
-    });
-  });
-}); // ensure invalid packages aren't listed
+    it('displays "empty" for User section', () => {
+      removeFakePackage('user', 'user-package')
+      listPackages([], () => {
+        const lines = console.log.argsForCall.map(arr => arr.join(' '))
+        expect(lines[0]).toMatch(/Built-in Atom Packages.*1/)
+        expect(lines[1]).toMatch(/test-module@1\.0\.0/)
+        expect(lines[3]).toMatch(/Dev Packages.*1/)
+        expect(lines[4]).toMatch(/dev-package@1\.0\.0/)
+        expect(lines[6]).toMatch(/Community Packages.*0/)
+        expect(lines[7]).toMatch(/(empty)/)
+        expect(lines[9]).toMatch(/Git Packages.*1/)
+        expect(lines[10]).toMatch(/git-package@1\.0\.0/)
+        expect(lines.join('\n')).not.toContain('.bin')
+      })
+    })
+  })
+})
