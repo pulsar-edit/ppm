3a86ba047ab0309c3d842aaa409a98bad306298f
GuilleW <devguillew@free.fr>
2022-11-10 00:40:45 +0100

    Decaffeinate `spec/packages-spec`

---
+++ spec/packages-spec.js
@@ -0,0 +1,24 @@
+const Packages = require('../lib/packages')
+
+describe('getRemote', () => {
+  it('returns origin if remote could not be determined', () => {
+    expect(Packages.getRemote()).toEqual('origin')
+  })
+
+  it('returns repository.url', () => {
+    const pack = {
+      repository: {
+        type: 'git',
+        url: 'https://github.com/atom/apm.git'
+      }
+    }
+    expect(Packages.getRemote(pack)).toEqual(pack.repository.url)
+  })
+
+  it('returns repository', () => {
+    const pack = {
+      repository: 'https://github.com/atom/apm'
+    }
+    expect(Packages.getRemote(pack)).toEqual(pack.repository)
+  })
+})
