018d74f3d8ff4023dd59fe580e6b81ffc29d84b3
GuilleW <devguillew@free.fr>
2022-11-10 00:40:45 +0100

    Decaffeinate `spec/packages-spec`

--- spec/packages-spec.js
+++ spec/packages-spec.js
@@ -1,26 +1,24 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
-const Packages = require('../lib/packages');
+const Packages = require('../lib/packages')
 
-describe('getRemote', function() {
-  it('returns origin if remote could not be determined', () => expect(Packages.getRemote()).toEqual('origin'));
+describe('getRemote', () => {
+  it('returns origin if remote could not be determined', () => {
+    expect(Packages.getRemote()).toEqual('origin')
+  })
 
-  it('returns repository.url', function() {
+  it('returns repository.url', () => {
     const pack = {
       repository: {
         type: 'git',
         url: 'https://github.com/atom/apm.git'
       }
-    };
-    return expect(Packages.getRemote(pack)).toEqual(pack.repository.url);
-  });
+    }
+    expect(Packages.getRemote(pack)).toEqual(pack.repository.url)
+  })
 
-  return it('returns repository', function() {
-    const pack =
-      {repository: 'https://github.com/atom/apm'};
-    return expect(Packages.getRemote(pack)).toEqual(pack.repository);
-  });
-});
+  it('returns repository', () => {
+    const pack = {
+      repository: 'https://github.com/atom/apm'
+    }
+    expect(Packages.getRemote(pack)).toEqual(pack.repository)
+  })
+})
