8b212a360e1588597f9aa97615a214b4cc1953dc
GuilleW <devguillew@free.fr>
2022-11-11 00:10:33 +0100

    Decaffeinate `spec/rebuild-spec`

--- spec/rebuild-spec.js
+++ spec/rebuild-spec.js
@@ -1,81 +1,69 @@
-/*
- * decaffeinate suggestions:
- * DS101: Remove unnecessary use of Array.from
- * DS102: Remove unnecessary code created because of implicit returns
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
-const path = require('path');
-const CSON = require('season');
-const fs = require('fs-plus');
-const temp = require('temp');
-const express = require('express');
-const http = require('http');
-const wrench = require('wrench');
-const apm = require('../lib/apm-cli');
+const path = require('path')
+const CSON = require('season')
+const fs = require('fs-plus')
+const temp = require('temp')
+const express = require('express')
+const http = require('http')
+const wrench = require('wrench')
+const apm = require('../lib/apm-cli')
 const {
   nodeVersion
 } = require('./config.json');
 
-describe('apm rebuild', function() {
-  let [server, originalPathEnv] = Array.from([]);
+describe('apm rebuild', () => {
+  var originalPathEnv, server
 
-  beforeEach(function() {
-    spyOnToken();
-    silenceOutput();
+  beforeEach(() => {
+    spyOnToken()
+    silenceOutput()
+    const app = express()
+    app.get(`/node/${nodeVersion}/node-${nodeVersion}-headers.tar.gz`, (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', `node-${nodeVersion}-headers.tar.gz`)));
+    app.get(`/node/${nodeVersion}/win-x86/node.lib`, (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', 'node.lib')));
+    app.get(`/node/${nodeVersion}/win-x64/node.lib`, (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', 'node_x64.lib')));
+    app.get(`/node/${nodeVersion}/SHASUMS256.txt`, (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', 'SHASUMS256.txt')));
+    server = http.createServer(app)
+    var live = false
+    server.listen(3000, '127.0.0.1', () => {
+      process.env.ATOM_HOME = temp.mkdirSync('apm-home-dir-')
+      process.env.ATOM_ELECTRON_URL = 'http://localhost:3000/node'
+      process.env.ATOM_PACKAGES_URL = 'http://localhost:3000/packages'
+      process.env.ATOM_ELECTRON_VERSION = nodeVersion
+      process.env.ATOM_RESOURCE_PATH = temp.mkdirSync('atom-resource-path-')
+      originalPathEnv = process.env.PATH
+      process.env.PATH = ''
+      live = true
+    })
+    waitsFor(() => live)
+  })
 
-    const app = express();
-    app.get(`/node/${nodeVersion}/node-${nodeVersion}-headers.tar.gz`, (request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', `node-${nodeVersion}-headers.tar.gz`)));
-    app.get(`/node/${nodeVersion}/win-x86/node.lib`, (request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', 'node.lib')));
-    app.get(`/node/${nodeVersion}/win-x64/node.lib`, (request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', 'node_x64.lib')));
-    app.get(`/node/${nodeVersion}/SHASUMS256.txt`, (request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', 'SHASUMS256.txt')));
+  afterEach(() => {
+    process.env.PATH = originalPathEnv
+    var done = false
+    server.close(() => {
+      done = true
+    })
+    return waitsFor(() => done)
+  })
 
-    server = http.createServer(app);
+  it('rebuilds all modules when no module names are specified', () => {
+    const packageToRebuild = path.join(__dirname, 'fixtures/package-with-native-deps')
+    process.chdir(packageToRebuild)
+    const callback = jasmine.createSpy('callback')
+    apm.run(['rebuild'], callback)
+    waitsFor('waiting for rebuild to complete', 600000, () => callback.callCount === 1)
+    runs(() => {
+      expect(callback.mostRecentCall.args[0]).toBeUndefined()
+    })
+  })
 
-    let live = false;
-    server.listen(3000, '127.0.0.1', function() {
-      const atomHome = temp.mkdirSync('apm-home-dir-');
-      process.env.ATOM_HOME = atomHome;
-      process.env.ATOM_ELECTRON_URL = "http://localhost:3000/node";
-      process.env.ATOM_PACKAGES_URL = "http://localhost:3000/packages";
-      process.env.ATOM_ELECTRON_VERSION = nodeVersion;
-      process.env.ATOM_RESOURCE_PATH = temp.mkdirSync('atom-resource-path-');
-
-      originalPathEnv = process.env.PATH;
-      process.env.PATH = "";
-      return live = true;
-    });
-    return waitsFor(() => live);
-  });
-
-  afterEach(function() {
-    process.env.PATH = originalPathEnv;
-
-    let done = false;
-    server.close(() => done = true);
-    return waitsFor(() => done);
-  });
-
-  it("rebuilds all modules when no module names are specified", function() {
-    const packageToRebuild = path.join(__dirname, 'fixtures/package-with-native-deps');
-
-    process.chdir(packageToRebuild);
-    const callback = jasmine.createSpy('callback');
-    apm.run(['rebuild'], callback);
-
-    waitsFor('waiting for rebuild to complete', 600000, () => callback.callCount === 1);
-
-    return runs(() => expect(callback.mostRecentCall.args[0]).toBeUndefined());
-  });
-
-  return it("rebuilds the specified modules", function() {
-    const packageToRebuild = path.join(__dirname, 'fixtures/package-with-native-deps');
-
-    process.chdir(packageToRebuild);
-    const callback = jasmine.createSpy('callback');
-    apm.run(['rebuild', 'native-dep'], callback);
-
-    waitsFor('waiting for rebuild to complete', 600000, () => callback.callCount === 1);
-
-    return runs(() => expect(callback.mostRecentCall.args[0]).toBeUndefined());
-  });
-});
+  it('rebuilds the specified modules', () => {
+    const packageToRebuild = path.join(__dirname, 'fixtures/package-with-native-deps')
+    process.chdir(packageToRebuild)
+    const callback = jasmine.createSpy('callback')
+    apm.run(['rebuild', 'native-dep'], callback)
+    waitsFor('waiting for rebuild to complete', 600000, () => callback.callCount === 1)
+    runs(() => {
+      expect(callback.mostRecentCall.args[0]).toBeUndefined()
+    })
+  })
+})
