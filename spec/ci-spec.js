1a6957aa4cc8bff7938b00330cb8d852d5e4b5bb
Polgár Márton <ersterpleghthome@gmail.com>
2023-03-28 17:53:33 +0200

    Adding the 'nodeVersion' development back

--- spec/ci-spec.js
+++ spec/ci-spec.js
@@ -17,45 +17,22 @@ describe('apm ci', () => {
     process.env.ATOM_RESOURCE_PATH = temp.mkdirSync('atom-resource-path-');
     delete process.env.npm_config_cache;
     const app = express();
-    app.get('/node/v10.20.1/node-v10.20.1.tar.gz', (request, response) => {
-      response.sendFile(path.join(__dirname, 'fixtures', 'node-v10.20.1.tar.gz'));
-    });
-    app.get('/node/v10.20.1/node-v10.20.1-headers.tar.gz', (request, response) => {
-      response.sendFile(path.join(__dirname, 'fixtures', 'node-v10.20.1-headers.tar.gz'));
-    });
-    app.get('/node/v10.20.1/node.lib', (request, response) => {
-      response.sendFile(path.join(__dirname, 'fixtures', 'node.lib'));
-    });
-    app.get('/node/v10.20.1/x64/node.lib', (request, response) => {
-      response.sendFile(path.join(__dirname, 'fixtures', 'node_x64.lib'));
-    });
-    app.get('/node/v10.20.1/SHASUMS256.txt', (request, response) => {
-      response.sendFile(path.join(__dirname, 'fixtures', 'SHASUMS256.txt'));
-    });
-    app.get('/test-module-with-dependencies', (request, response) => {
-      response.sendFile(path.join(__dirname, 'fixtures', 'install-locked-version.json'));
-    });
-    app.get('/test-module', (request, response) => {
-      response.sendFile(path.join(__dirname, 'fixtures', 'install-test-module.json'));
-    });
-    app.get('/native-module', (request, response) => {
-      response.sendFile(path.join(__dirname, 'fixtures', 'native-module.json'));
-    });
-    app.get('/tarball/test-module-with-dependencies-1.1.0.tgz', (request, response) => {
-      response.sendFile(path.join(__dirname, 'fixtures', 'test-module-with-dependencies-1.1.0.tgz'));
-    });
-    app.get('/tarball/test-module-1.1.0.tgz', (request, response) => {
-      response.sendFile(path.join(__dirname, 'fixtures', 'test-module-1.1.0.tgz'));
-    });
-    app.get('/tarball/native-module-1.0.0.tgz', (request, response) => {
-      response.sendFile(path.join(__dirname, 'fixtures', 'native-module-1.0.0.tgz'));
-    });
+    app.get(`/node/${nodeVersion}/node-${nodeVersion}-headers.tar.gz`, (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', `node-${nodeVersion}-headers.tar.gz`)));
+    app.get(`/node/${nodeVersion}/win-x86/node.lib`, (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', 'node.lib')));
+    app.get(`/node/${nodeVersion}/win-x64/node.lib`, (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', 'node_x64.lib')));
+    app.get(`/node/${nodeVersion}/SHASUMS256.txt`, (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'node-dist', 'SHASUMS256.txt')));
+    app.get('/test-module-with-dependencies', (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'install-locked-version.json')));
+    app.get('/test-module', (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'install-test-module.json')));
+    app.get('/native-module', (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'native-module.json')));
+    app.get('/tarball/test-module-with-dependencies-1.1.0.tgz', (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'test-module-with-dependencies-1.1.0.tgz')));
+    app.get('/tarball/test-module-1.1.0.tgz', (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'test-module-1.1.0.tgz')));
+    app.get('/tarball/native-module-1.0.0.tgz', (_request, response) => response.sendFile(path.join(__dirname, 'fixtures', 'native-module-1.0.0.tgz')));
     server = http.createServer(app);
     var live = false;
     server.listen(3000, '127.0.0.1', () => {
       process.env.ATOM_ELECTRON_URL = 'http://localhost:3000/node';
       process.env.ATOM_PACKAGES_URL = 'http://localhost:3000/packages';
-      process.env.ATOM_ELECTRON_VERSION = 'v10.20.1';
+      process.env.ATOM_ELECTRON_VERSION = nodeVersion;
       process.env.npm_config_registry = 'http://localhost:3000/';
       live = true;
     });
