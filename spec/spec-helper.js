dddbe3163ff09bfc9d88f5c8dd768ba35d63d6d4
Polgár Márton <ersterpleghthome@gmail.com>
2023-03-28 13:26:24 +0200

    Keep the semicolons

--- spec/spec-helper.js
+++ spec/spec-helper.js
@@ -1,17 +1,17 @@
-const auth = require('../lib/auth')
+const auth = require('../lib/auth');
 
 global.silenceOutput = (callThrough = false) => {
-  spyOn(console, 'log')
-  spyOn(console, 'error')
-  spyOn(process.stdout, 'write')
-  spyOn(process.stderr, 'write')
+  spyOn(console, 'log');
+  spyOn(console, 'error');
+  spyOn(process.stdout, 'write');
+  spyOn(process.stderr, 'write');
 
   if (callThrough) {
-    const ref = [console.log, console.error, process.stdout.write, process.stderr.write]
+    const ref = [console.log, console.error, process.stdout.write, process.stderr.write];
     for (const spy of ref) {
-      spy.andCallThrough()
+      spy.andCallThrough();
     }
   }
-}
+};
 
-global.spyOnToken = () => spyOn(auth, 'getToken').andCallFake(callback => callback(null, 'token'))
+global.spyOnToken = () => spyOn(auth, 'getToken').andCallFake(callback => callback(null, 'token'));
