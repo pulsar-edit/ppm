672a0aed337c5d8c6749144c83ed63d66209af14
Polgár Márton <ersterpleghthome@gmail.com>
2023-05-21 10:33:27 +0200

    Bringing semicolons back

--- spec/help-spec.js
+++ spec/help-spec.js
@@ -1,56 +1,56 @@
-const apm = require('../lib/apm-cli')
+const apm = require('../lib/apm-cli');
 
 describe('command help', () => {
   beforeEach(() => {
-    spyOnToken()
-    silenceOutput()
-  })
+    spyOnToken();
+    silenceOutput();
+  });
 
   describe('apm help publish', () => {
     it('displays the help for the command', () => {
-      const callback = jasmine.createSpy('callback')
-      apm.run(['help', 'publish'], callback)
-      waitsFor('waiting for help to complete', 60000, () => callback.callCount === 1)
+      const callback = jasmine.createSpy('callback');
+      apm.run(['help', 'publish'], callback);
+      waitsFor('waiting for help to complete', 60000, () => callback.callCount === 1);
       runs(() => {
-        expect(console.error.callCount).toBeGreaterThan(0)
-        expect(callback.mostRecentCall.args[0]).toBeUndefined()
-      })
-    })
-  })
+        expect(console.error.callCount).toBeGreaterThan(0);
+        expect(callback.mostRecentCall.args[0]).toBeUndefined();
+      });
+    });
+  });
 
   describe('apm publish -h', () => {
     it('displays the help for the command', () => {
-      const callback = jasmine.createSpy('callback')
-      apm.run(['publish', '-h'], callback)
-      waitsFor('waiting for help to complete', 60000, () => callback.callCount === 1)
+      const callback = jasmine.createSpy('callback');
+      apm.run(['publish', '-h'], callback);
+      waitsFor('waiting for help to complete', 60000, () => callback.callCount === 1);
       runs(() => {
-        expect(console.error.callCount).toBeGreaterThan(0)
-        expect(callback.mostRecentCall.args[0]).toBeUndefined()
-      })
-    })
-  })
+        expect(console.error.callCount).toBeGreaterThan(0);
+        expect(callback.mostRecentCall.args[0]).toBeUndefined();
+      });
+    });
+  });
 
   describe('apm help', () => {
     it('displays the help for apm', () => {
-      const callback = jasmine.createSpy('callback')
-      apm.run(['help'], callback)
-      waitsFor('waiting for help to complete', 60000, () => callback.callCount === 1)
+      const callback = jasmine.createSpy('callback');
+      apm.run(['help'], callback);
+      waitsFor('waiting for help to complete', 60000, () => callback.callCount === 1);
       runs(() => {
-        expect(console.error.callCount).toBeGreaterThan(0)
-        expect(callback.mostRecentCall.args[0]).toBeUndefined()
-      })
-    })
-  })
+        expect(console.error.callCount).toBeGreaterThan(0);
+        expect(callback.mostRecentCall.args[0]).toBeUndefined();
+      });
+    });
+  });
 
   describe('apm', () => {
     it('displays the help for apm', () => {
-      const callback = jasmine.createSpy('callback')
-      apm.run([], callback)
-      waitsFor('waiting for help to complete', 60000, () => callback.callCount === 1)
+      const callback = jasmine.createSpy('callback');
+      apm.run([], callback);
+      waitsFor('waiting for help to complete', 60000, () => callback.callCount === 1);
       runs(() => {
-        expect(console.error.callCount).toBeGreaterThan(0)
-        expect(callback.mostRecentCall.args[0]).toBeUndefined()
-      })
-    })
-  })
-})
+        expect(console.error.callCount).toBeGreaterThan(0);
+        expect(callback.mostRecentCall.args[0]).toBeUndefined();
+      });
+    });
+  });
+});
