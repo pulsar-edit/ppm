03bc18b02ad3f6578a1149dc8c7b3cc8d869609e
confused_techie <dev@lhbasics.com>
2023-05-20 22:06:28 -0700

    Added some breathing room

--- spec/featured-spec.js
+++ spec/featured-spec.js
@@ -10,18 +10,23 @@ describe('apm featured', () => {
     silenceOutput();
     spyOnToken();
     const app = express();
+    
     app.get('/packages/featured', (request, response) => {
       response.sendFile(path.join(__dirname, 'fixtures', 'packages.json'));
     });
+    
     app.get('/themes/featured', (request, response) => {
       response.sendFile(path.join(__dirname, 'fixtures', 'themes.json'));
     });
+    
     server = http.createServer(app);
     let live = false;
+    
     server.listen(3000, '127.0.0.1', () => {
       process.env.ATOM_API_URL = 'http://localhost:3000';
       live = true;
     });
+    
     waitsFor(() => live);
   });
 
