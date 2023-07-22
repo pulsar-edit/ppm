1e8edbc38fb39616af03280d16e1ba4fb03b6d75
confused-Techie <dev@lhbasics.com>
2023-07-22 16:40:59 -0700

    Decaf `cli.js`

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,4 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
+
 const apm = require('./apm-cli');
 
 process.title = 'apm';
