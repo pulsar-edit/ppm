73006864413ff0fbb903d7c618dbd3baea6cf330
confused-Techie <dev@lhbasics.com>
2023-07-22 17:05:51 -0700

    Decaf `config.js`

--- src/config.js
+++ src/config.js
@@ -1,10 +1,4 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS206: Consider reworking classes to avoid initClass
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
+
 let Config;
 const path = require('path');
 const _ = require('underscore-plus');
@@ -13,57 +7,51 @@ const apm = require('./apm');
 const Command = require('./command');
 
 module.exports =
-(Config = (function() {
-  Config = class Config extends Command {
-    static initClass() {
-      this.commandNames = ['config'];
-    }
+class Config extends Command {
+  static commandNames = [ "config" ];
 
-    constructor() {
-      super();
-      const atomDirectory = apm.getAtomDirectory();
-      this.atomNodeDirectory = path.join(atomDirectory, '.node-gyp');
-      this.atomNpmPath = require.resolve('npm/bin/npm-cli');
-    }
+  constructor() {
+    super();
+    const atomDirectory = apm.getAtomDirectory();
+    this.atomNodeDirectory = path.join(atomDirectory, '.node-gyp');
+    this.atomNpmPath = require.resolve('npm/bin/npm-cli');
+  }
 
-    parseOptions(argv) {
-      const options = yargs(argv).wrap(Math.min(100, yargs.terminalWidth()));
-      options.usage(`\
+  parseOptions(argv) {
+    const options = yargs(argv).wrap(Math.min(100, yargs.terminalWidth()));
+    options.usage(`\
 
-Usage: ppm config set <key> <value>
-       ppm config get <key>
-       ppm config delete <key>
-       ppm config list
-       ppm config edit
+      Usage: ppm config set <key> <value>
+             ppm config get <key>
+             ppm config delete <key>
+             ppm config list
+             ppm config edit
 \
 `
-      );
-      return options.alias('h', 'help').describe('help', 'Print this usage message');
-    }
+    );
+    return options.alias('h', 'help').describe('help', 'Print this usage message');
+  }
 
-    run(options) {
-      const {callback} = options;
-      options = this.parseOptions(options.commandArgs);
+  run(options) {
+    const {callback} = options;
+    options = this.parseOptions(options.commandArgs);
 
-      let configArgs = ['--globalconfig', apm.getGlobalConfigPath(), '--userconfig', apm.getUserConfigPath(), 'config'];
-      configArgs = configArgs.concat(options.argv._);
+    let configArgs = ['--globalconfig', apm.getGlobalConfigPath(), '--userconfig', apm.getUserConfigPath(), 'config'];
+    configArgs = configArgs.concat(options.argv._);
 
-      const env = _.extend({}, process.env, {HOME: this.atomNodeDirectory, RUSTUP_HOME: apm.getRustupHomeDirPath()});
-      const configOptions = {env};
+    const env = _.extend({}, process.env, {HOME: this.atomNodeDirectory, RUSTUP_HOME: apm.getRustupHomeDirPath()});
+    const configOptions = {env};
 
-      return this.fork(this.atomNpmPath, configArgs, configOptions, function(code, stderr, stdout) {
-        if (stderr == null) { stderr = ''; }
-        if (stdout == null) { stdout = ''; }
-        if (code === 0) {
-          if (stdout) { process.stdout.write(stdout); }
-          return callback();
-        } else {
-          if (stderr) { process.stdout.write(stderr); }
-          return callback(new Error(`npm config failed: ${code}`));
-        }
-      });
-    }
-  };
-  Config.initClass();
-  return Config;
-})());
+    return this.fork(this.atomNpmPath, configArgs, configOptions, function(code, stderr, stdout) {
+      if (stderr == null) { stderr = ''; }
+      if (stdout == null) { stdout = ''; }
+      if (code === 0) {
+        if (stdout) { process.stdout.write(stdout); }
+        return callback();
+      } else {
+        if (stderr) { process.stdout.write(stderr); }
+        return callback(new Error(`npm config failed: ${code}`));
+      }
+    });
+  }
+}
