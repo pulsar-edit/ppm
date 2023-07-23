4586429910dfbc76250dc8eb29fcaf8668216dda
confused-Techie <dev@lhbasics.com>
2023-07-22 17:10:21 -0700

    decaf `deprecated-packages`

--- src/deprecated-packages.js
+++ src/deprecated-packages.js
@@ -1,16 +1,9 @@
-/*
- * decaffeinate suggestions:
- * DS102: Remove unnecessary code created because of implicit returns
- * DS104: Avoid inline assignments
- * DS207: Consider shorter variations of null checks
- * Full docs: https://github.com/decaffeinate/decaffeinate/blob/main/docs/suggestions.md
- */
+
 const semver = require('semver');
 let deprecatedPackages = null;
 
 exports.isDeprecatedPackage = function(name, version) {
-  if (deprecatedPackages == null) { let left;
-  deprecatedPackages = (left = require('../deprecated-packages')) != null ? left : {}; }
+  deprecatedPackages ??= require("../deprecated-packages") ?? {};
   if (!deprecatedPackages.hasOwnProperty(name)) { return false; }
 
   const deprecatedVersionRange = deprecatedPackages[name].version;
