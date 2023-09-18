11b6f14bab7c24f7f95389297834a62175ff2490
Polgár Márton <ersterpleghthome@gmail.com>
2023-09-18 18:27:48 +0200

    Promisification of enable.js Tests passing. Trivial wrapper added.

--- src/enable.js
+++ src/enable.js
@@ -9,6 +9,7 @@ const Command = require('./command');
 
 module.exports =
 class Enable extends Command {
+  static promiseBased = true;
   static commandNames = [ "enable" ];
 
     parseOptions(argv) {
@@ -24,52 +25,53 @@ Enables the named package(s).\
     }
 
     run(options) {
-      let error, settings;
-      const {callback} = options;
-      options = this.parseOptions(options.commandArgs);
-      let packageNames = this.packageNamesFromArgv(options.argv);
+      return new Promise((resolve, _reject) => {
+        let settings;
+        options = this.parseOptions(options.commandArgs);
+        let packageNames = this.packageNamesFromArgv(options.argv);
 
-      const configFilePath = CSON.resolve(path.join(config.getAtomDirectory(), 'config'));
-      if (!configFilePath) {
-        callback("Could not find config.cson. Run Atom first?");
-        return;
-      }
+        const configFilePath = CSON.resolve(path.join(config.getAtomDirectory(), 'config'));
+        if (!configFilePath) {
+          resolve("Could not find config.cson. Run Atom first?");
+          return;
+        }
 
-      try {
-        settings = CSON.readFileSync(configFilePath);
-      } catch (error) {
-        callback(`Failed to load \`${configFilePath}\`: ${error.message}`);
-        return;
-      }
+        try {
+          settings = CSON.readFileSync(configFilePath);
+        } catch (error) {
+          resolve(`Failed to load \`${configFilePath}\`: ${error.message}`);
+          return;
+        }
 
-      const keyPath = '*.core.disabledPackages';
-      const disabledPackages = _.valueForKeyPath(settings, keyPath) ?? [];
+        const keyPath = '*.core.disabledPackages';
+        const disabledPackages = _.valueForKeyPath(settings, keyPath) ?? [];
 
-      const errorPackages = _.difference(packageNames, disabledPackages);
-      if (errorPackages.length > 0) {
-        console.log(`Not Disabled:\n  ${errorPackages.join('\n  ')}`);
-      }
+        const errorPackages = _.difference(packageNames, disabledPackages);
+        if (errorPackages.length > 0) {
+          console.log(`Not Disabled:\n  ${errorPackages.join('\n  ')}`);
+        }
 
-      // can't enable a package that isn't disabled
-      packageNames = _.difference(packageNames, errorPackages);
+        // can't enable a package that isn't disabled
+        packageNames = _.difference(packageNames, errorPackages);
 
-      if (packageNames.length === 0) {
-        callback("Please specify a package to enable");
-        return;
-      }
+        if (packageNames.length === 0) {
+          resolve("Please specify a package to enable");
+          return;
+        }
 
-      const result = _.difference(disabledPackages, packageNames);
-      _.setValueForKeyPath(settings, keyPath, result);
+        const result = _.difference(disabledPackages, packageNames);
+        _.setValueForKeyPath(settings, keyPath, result);
 
-      try {
-        CSON.writeFileSync(configFilePath, settings);
-      } catch (error) {
-        callback(`Failed to save \`${configFilePath}\`: ${error.message}`);
-        return;
-      }
+        try {
+          CSON.writeFileSync(configFilePath, settings);
+        } catch (error) {
+          resolve(`Failed to save \`${configFilePath}\`: ${error.message}`);
+          return;
+        }
 
-      console.log(`Enabled:\n  ${packageNames.join('\n  ')}`);
-      this.logSuccess();
-      return callback();
+        console.log(`Enabled:\n  ${packageNames.join('\n  ')}`);
+        this.logSuccess();
+        resolve();
+      });
     }
   }
