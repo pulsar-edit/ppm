f4f1e06cff9b9ef73ad385a87b72465b1ad62efc
confused_techie <dev@lhbasics.com>
2023-12-14 07:46:38 -0800

    Migrate from `rimraf` to NodeJS `fs`

--- src/fs.js
+++ src/fs.js
@@ -2,7 +2,6 @@
 const _ = require('underscore-plus');
 const fs = require('fs-plus');
 const ncp = require('ncp');
-const rm = require('rimraf');
 const wrench = require('wrench');
 const path = require('path');
 
@@ -25,23 +24,21 @@ const fsAdditions = {
 
   cp(sourcePath, destinationPath) {
     return new Promise((resolve, reject) => {
-      rm(destinationPath, error => {
-        if (error != null) {
-          return reject(error);
-        }
+      fs.rm(destinationPath, { recursive: true, force: true }).then(() => {
         ncp(sourcePath, destinationPath, (error, value) => void (error != null ? reject(error) : resolve(value)));
+      }).catch((error) => {
+        return reject(error);
       });
     });
   },
 
   mv(sourcePath, destinationPath) {
     return new Promise((resolve, reject) => {
-      rm(destinationPath, error => {
-        if (error != null) {
-          return reject(error);
-        }
+      fs.rm(destinationPath, { recursive: true, force: true }).then(() => {
         wrench.mkdirSyncRecursive(path.dirname(destinationPath), 0o755);
         fs.rename(sourcePath, destinationPath, (error, value) => void (error != null ? reject(error) : resolve(value)));
+      }).catch((error) => {
+        return reject(error);
       });
     });
   }
